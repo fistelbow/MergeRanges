e712b839ec7bcf2b925fffee99f11d610cbbcc3f
Tony Ambrose <fistelbow@gmail.com>
2018-06-20 02:01:24 -0700

    Add project files.

---
+++ MergeRanges/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+
+namespace MergeRanges
+{
+    /// <summary>
+    /// MergeRanges console application to merge a collection of integer intervals, which represent
+    /// value ranges, into the most compact yet equivalent set of ranges covering the same intervals.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main method to run workflow. Loads a JSON file containing integer intervals with Start and Stop values.
+        /// </summary>
+        /// <param name="args">Expecting optional string arg[0] that is a path to a valid JSON file. 
+        /// If not provided, defaults to a project data file.</param>
+        static void Main(string[] args)
+        {
+            string path = ((args.Length == 0) ? @".\SampleRanges3.json" : args[0]); // Default JSON files in $(OutDir)
+            RangeCollection coll = new RangeCollection();
+
+            if (File.Exists(path))
+            {
+                using (Stream stream = File.OpenRead(path))
+                {
+                    var serializer = new DataContractJsonSerializer(typeof(RangeCollection));
+                    coll = (RangeCollection)serializer.ReadObject(stream);
+                }
+
+                List<Tuple<int, int>> tuples = new List<Tuple<int, int>>(); 
+
+                foreach (Range range in coll.Ranges)
+                {
+                    bool startIsInt;
+                    bool stopIsInt;
+
+                    startIsInt = Int32.TryParse(range.Start, out int start); // Confirm each value is an integer
+                    stopIsInt = Int32.TryParse(range.Stop, out int stop);
+
+                    if (startIsInt && stopIsInt)
+                    {
+                        tuples.Add(Tuple.Create(start, stop));
+                    }
+                }
+
+                RangeList rangeList = new RangeList(tuples);
+                rangeList.PrintTuples("Unsorted");
+                rangeList.Sort();
+                rangeList.PrintTuples("Sorted");
+                rangeList.Merge();
+                rangeList.PrintTuples("Merged");
+            }
+        }
+
+
+        [DataContract]
+        class Range
+        {
+            [DataMember(Name = "Start")] public string Start { get; set; }
+            [DataMember(Name = "Stop")] public string Stop { get; set; }
+        }
+
+        [DataContract]
+        class RangeCollection
+        {
+            [DataMember(Name = "Ranges")] public IEnumerable<Range> Ranges { get; set; }
+        }
+    }
+}
---
+++ MergeRanges/RangeList.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeRanges
+{
+    /// <summary>
+    /// RangeList class encapsulates functionality to load, sort and merge a list of intervals.
+    /// </summary>
+    public class RangeList
+    {
+        public List<Tuple<int, int>> Tuples { get; set; }
+
+        /// <summary>
+        /// Constructor from input list of tuples
+        /// </summary>
+        /// <param name="tuples"></param>
+        public RangeList(List<Tuple<int, int>> tuples)
+        {
+            this.Tuples = tuples;
+        }
+
+        /// <summary>
+        /// Constructor from an array of tuples
+        /// </summary>
+        /// <param name="tuples"></param>
+        public RangeList(Tuple<int, int>[] tuples)
+        {
+            this.Tuples = tuples.ToList();
+        }
+
+        /// <summary>
+        /// Constructor from a 2D array of integers
+        /// </summary>
+        /// <param name="tuples"></param>
+        public RangeList(int[,] tuples)
+        {
+            List<Tuple<int, int>> convert = new List<Tuple<int, int>>();
+
+            for (int i = 0; i < tuples.Length; i++)
+            {
+                int item1 = tuples[i,0];
+                int item2 = tuples[i,1];
+                Tuple<int, int> tuple = new Tuple<int, int>(item1, item2);
+                convert.Add(tuple);
+            }
+
+            this.Tuples = convert;
+        }
+
+        /// <summary>
+        /// Wrapper method to expose RangeList sorting.
+        /// </summary>
+        public void Sort()
+        {
+            Tuples.Sort();
+        }
+
+        /// <summary>
+        /// Core logic for merge algorithm. Iterates through a sorted RangeList, comparing Start value
+        /// of the current tuple with the Stop value of the previous tuple. If greater, it regards
+        /// the prevous tuple as non-overlapping and adds it to an output list. If less or equal, it
+        /// merges previous with current, using the Start from previous and the greater of the two
+        /// Stop values from previous and current.
+        /// </summary>
+        public void Merge()
+        {
+            Tuple<int, int> lastTuple = Tuples.FirstOrDefault();
+            List<Tuple<int, int>> merged = new List<Tuple<int, int>>();
+
+            foreach (Tuple<int, int> currTuple in Tuples)
+            {
+                if (currTuple.Item1 > lastTuple.Item2)
+                {
+                    merged.Add(lastTuple);
+                    lastTuple = currTuple;
+                }
+                else
+                {
+                    lastTuple = Tuple.Create(lastTuple.Item1, Math.Max(lastTuple.Item2, currTuple.Item2));
+                }
+            }
+
+            merged.Add(lastTuple);
+            Tuples = merged;
+        }
+
+        /// <summary>
+        /// Writes list of tuples to the console.
+        /// </summary>
+        /// <param name="prefix"></param>
+        public void PrintTuples(string prefix)
+        {
+            foreach (Tuple<int, int> tuple in Tuples)
+            {
+                Console.WriteLine("{0} Range start: {1}, Range stop: {2}", prefix, tuple.Item1, tuple.Item2);
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
